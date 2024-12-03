c29ab4bd68a11b5faad55821f33cd03cf38a06f1
AlexandrYurkov <72255116+AlexandrYurkov@users.noreply.github.com>
2024-12-03 11:41:55 +0300

    Revert "123"

--- ChatService/dbinit.sql
+++
@@ -1,5 +0,0 @@
-create table users (id integer primary key, login varchar(255), password varchar(255), nickname varchar(255));
-create table bonuses (id integer primary key, owner_login varchar(255), amount int);
-create table test (id integer primary key, owner_login varchar(255), amount int);
-create table test (id integer primary key, owner_login varchar(255), amount int);
-create table bonuses (id integer primary key, owner_login varchar(255), amount int);
